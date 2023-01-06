9675b8186ad6fd79ced85f2fb287dd04d8b7e169
coderHDY <huangdeyu2020@163.com>
2023-01-06 11:04:58 +0800

    useLocalStorage完成

--- 21_useLocalStorage/components/About.jsx
+++ 21_useLocalStorage/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useResolvedPath } from 'react-router-dom';
 import useLocalStorage from '../utils/useLocalStorage';
 
@@ -16,14 +16,40 @@ export default function About() {
             }
         )
     }
-    const [time] = useLocalStorage("time");
-    console.log(`about获取到的：${time}`);
+    const [val] = useLocalStorage("time");
+    const time = val ? +val : 0;
+
+    /*  */
+    const [num, setNum] = useState(0);
+    useEffect(() => {
+        window.addEventListener("doing", () => {
+            setNum(c => c + 1);
+        })
+    }, []);
+    const dispatch = () => {
+        const e = new CustomEvent("doing", { detail: { user: "hdy" } });
+        dispatchEvent(e);
+    }
     return (
         <>
             <div>About</div>
             <div>{`${parseInt(time / 60)}: ${parseInt(time % 60)}`}</div>
             <div>{time}</div>
             <button onClick={goHome}>点我去主页</button>
+            <Child />
+            <div>{num}</div>
+            <button onClick={dispatch}>点我发射自定义事件</button>
         </>
     )
+}
+
+const Child = () => {
+    const [time] = useLocalStorage("time");
+
+    /*  */
+    const [num, setNum] = useState(0);
+    useEffect(() => {
+        window.addEventListener("doing", () => setNum(c => c + 1));
+    }, [])
+    return <div>children: {num}</div>
 }
