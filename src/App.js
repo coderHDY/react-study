29d49bc7c8c90cfd7e1cd95126fedd0133c160c2
coder-HDY <d_huang@dreamarts.co.jp>
2023-03-15 22:08:20 +0900

    font icon 追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routerMap from './router'
+// import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, useTheme } from '@material-ui/core/styles';
+
+const theme = rootSize => ({
+    dark: {
+        textColor: "#333333",
+        fontSize: `${rootSize * 1}px`,
+    },
+    light: {
+        textColor: "#ffffff",
+        fontSize: `${rootSize * 1}px`,
+    }
+});
 
 function App() {
     const elements = useRoutes(routerMap);
+    const [mode, setMode] = useState("dark");
+    const [rootSize, setRootSize] = useState(16);
     return (
-        <div>
-            {elements}
-        </div>
+        <ThemeProvider theme={theme(rootSize)[mode]}>
+            <div>
+                <button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>切换</button>
+                <button onClick={() => setRootSize(c => c + 1)}>字体变大</button>
+                {elements}
+            </div>
+        </ThemeProvider>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,82 +1,25 @@
-import React from "react";
-import Nav from "./Nav";
-import {
-    Button,
-    Grid,
-} from "@mui/material";
-import styled, {
-    keyframes,
-} from "styled-components";
+import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-const Btn = styled.button`
-    background-color: ${props => props.bgc};
-    color: #fff;
-`
+export default function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-const BlackBtn = styled(Btn)`
-    {...props}
-`
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
 
-const RedBtn = styled(Button).attrs({
-    variant: "contained",
-})`
-    && {
-        border: 2px solid aqua;
-    }
-`
-
-const rotateAnimation = keyframes`
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-`
-const RotateRedBtn = styled(Button)`
-    && {
-        animation: ${rotateAnimation} 1s infinite linear;
-    }
-`
-
-const ReverseBtn = props => <Btn {...props}>{props.children.split("").reverse().join("")}</Btn>
-
-export default function Home() {
-    return (
-        <>
-            <Nav /> 
-            <Grid
-                container
-                columns={12}
-                columnSpacing={1}
-                sx={{
-                    backgroundColor: "green",
-                    height: "calc(100vh - 56px)"
-                }}
-            >
-                <Grid
-                    item
-                    display={{xs: "none", md: "block"}}
-                    xs={false}
-                    md={3}
-                    bgcolor="red"
-                    >
-                    <Button variant="contained">Hello World!!!</Button>
-                </Grid>
-
-                <Grid item xs={12} md={9} bgcolor="#fff">
-                    <Btn bgc="red">Hello World</Btn>
-                    
-                    <Btn bgc="blue">Hello World</Btn>
-                    
-                    <BlackBtn bgc="rgb(196 161 239)" color="black">Hello World</BlackBtn>
-
-                    <BlackBtn as={ReverseBtn} bgc="rgb(196 161 239)" color="black">Hello World</BlackBtn>
-
-                    <RedBtn>Hello World!!</RedBtn>
-                    <RotateRedBtn variant="contained">Hello World!!</RotateRedBtn>
-                </Grid>
-            </Grid>
-        </>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      1
+    </ThemeProvider>
+  );
 }
