425814991cba1fe0390fa5c145e90c02360172f3
coder-HDY <d_huang@dreamarts.co.jp>
2023-03-18 14:22:35 +0900

    awsome-MUI-drawer

--- src/App.js
+++ src/App.js
@@ -4,29 +4,18 @@ import routerMap from './router'
 // import { ThemeProvider } from 'styled-components';
 import { ThemeProvider, useTheme } from '@material-ui/core/styles';
 
-const theme = rootSize => ({
-    dark: {
-        textColor: "#333333",
-        fontSize: `${rootSize * 1}px`,
-    },
-    light: {
-        textColor: "#ffffff",
-        fontSize: `${rootSize * 1}px`,
-    }
-});
-
 function App() {
     const elements = useRoutes(routerMap);
     const [mode, setMode] = useState("dark");
     const [rootSize, setRootSize] = useState(16);
     return (
-        <ThemeProvider theme={theme(rootSize)[mode]}>
-            <div>
-                <button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>切换</button>
-                <button onClick={() => setRootSize(c => c + 1)}>字体变大</button>
-                {elements}
-            </div>
-        </ThemeProvider>
+        // <ThemeProvider theme={theme(rootSize)[mode]}>
+        <div>
+            <button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>切换</button>
+            <button onClick={() => setRootSize(c => c + 1)}>字体变大</button>
+            {elements}
+        </div>
+        // </ThemeProvider>
     )
 }
 
