316cf7431cf1a160527dd8db452aed0a88547716
coder-HDY <d_huang@dreamarts.co.jp>
2023-03-28 08:42:58 +0900

    react-use

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import routerMap from './router'
 // import { ThemeProvider } from 'styled-components';
-import { ThemeProvider, useTheme } from '@material-ui/core/styles';
 
 function App() {
     const elements = useRoutes(routerMap);
-    const [mode, setMode] = useState("dark");
-    const [rootSize, setRootSize] = useState(16);
     return (
-        // <ThemeProvider theme={theme(rootSize)[mode]}>
         <div>
-            <button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>切换</button>
-            <button onClick={() => setRootSize(c => c + 1)}>字体变大</button>
             {elements}
         </div>
-        // </ThemeProvider>
     )
 }
 
