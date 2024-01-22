e8c05d433f3f7b5b7258a7ce32695f2e15413ab5
coderHDY <986005715@qq.com>
2024-01-22 14:27:54 +0800

    feat: react-window

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,21 @@
 // @monaco-editor/react + ajv
 import React, { useState, useEffect, useRef } from "react";
-import {
-  Gantt,
-  Task,
-  EventOption,
-  StylingOption,
-  ViewMode,
-  DisplayOption,
-} from "gantt-task-react";
-import "gantt-task-react/dist/index.css";
-
-const randomDate = () => {
-  return randomDateFrom(new Date(2020, 0, 1), new Date());
-};
-const randomDateFrom = (start, end = new Date()) => {
-  return new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-};
-let tasks = Array(100)
-  .fill()
-  .map((item, i) => {
-    const start = randomDate();
-    const end = randomDateFrom(start);
-    return {
-      start,
-      end,
-      name: "Idea",
-      id: `Task ${i}`,
-      type: "task",
-      progress: 45,
-      isDisabled: true,
-      styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
-    };
-  });
-const Home = () => {
-  return (
-    <>
-      <h2>Home</h2>
-      <Gantt tasks={tasks} />
-    </>
-  );
-};
-
+import { FixedSizeList as List } from 'react-window';
+ 
+const Row = ({ index, style }) => (
+  <div style={style}>
+    Row {index}
+  </div>
+);
+ 
+const Home = () => (
+  <List
+    height={1000}
+    itemCount={1000}
+    itemSize={35}
+    scrollTop={150}
+  >
+    {Row}
+  </List>
+);
 export default Home;
